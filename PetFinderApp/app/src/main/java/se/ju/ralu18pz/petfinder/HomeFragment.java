f4076a3cd7ed098a0fbf77ca7d706805e12ec76b
LuisitoRady <[crdluis97@.gmail.com]>
2019-02-25 12:18:17 +0100

    Added fragments

---
+++ PetFinderApp/app/src/main/java/se/ju/ralu18pz/petfinder/HomeFragment.java
@@ -0,0 +1,34 @@
+package se.ju.ralu18pz.petfinder;
+
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.text.method.ScrollingMovementMethod;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+public class HomeFragment extends Fragment {
+
+    private TextView infoText;
+
+    public HomeFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_home, container, false);
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        infoText = (TextView) getView().findViewById(R.id.info_text);
+        infoText.setMovementMethod(new ScrollingMovementMethod());
+    }
+}
--- PetFinderApp/app/src/main/java/se/ju/ralu18pz/petfinder/MainActivity.java
+++ PetFinderApp/app/src/main/java/se/ju/ralu18pz/petfinder/MainActivity.java
@@ -1,13 +1,68 @@
 package se.ju.ralu18pz.petfinder;
 
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.method.ScrollingMovementMethod;
+import android.view.MenuItem;
+import android.widget.FrameLayout;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private BottomNavigationView mainNav;
+    private FrameLayout mainFrame;
+
+    private HomeFragment homeFragment;
+    private MapFragment mapFragment;
+    private PetsFragment petsFragment;
+    private ProfileFragment profileFragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mainFrame = (FrameLayout) findViewById(R.id.main_frame);
+        mainNav = (BottomNavigationView) findViewById(R.id.main_nav);
+
+        homeFragment = new HomeFragment();
+        mapFragment = new MapFragment();
+        petsFragment = new PetsFragment();
+        profileFragment = new ProfileFragment();
+
+        setFragment(homeFragment);
+
+        mainNav.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                switch (menuItem.getItemId()) {
+                    case R.id.home_button:
+                        setFragment(homeFragment);
+                        return true;
+                    case R.id.map_button:
+                        setFragment(mapFragment);
+                        return true;
+                    case R.id.pets_button:
+                        setFragment(petsFragment);
+                        return true;
+                    case R.id.profile_button:
+                        setFragment(profileFragment);
+                        return true;
+
+                        default:
+                            return false;
+                }
+            }
+        });
+    }
+
+    private void setFragment(Fragment fragment) {
+        FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+        fragmentTransaction.replace(R.id.main_frame, fragment);
+        fragmentTransaction.commit();
     }
 }
